817f4e45332e693097b66380bb7667aa9199f762
RainyFluff <nikodemius.ivarsson@elev.ga.lbs.se>
2020-12-09 15:02:40 +0100

    v.50
    Shooting go noobing

---
+++ Quek-Git/Assets/Scripts/Shooting.cs
@@ -0,0 +1,42 @@
+﻿
+using UnityEngine;
+
+public class Shooting : MonoBehaviour
+{
+    public float damage = 10f;
+    public float range = 100f;
+    public Camera fpsCam;
+    public float fireRate = 15f;
+
+    private float nextTimeToFire = 0f;
+
+    // Update is called once per frame
+    void Update()
+    {
+        if (Input.GetButtonDown("Fire1") && Time.time >= nextTimeToFire)
+        {
+            nextTimeToFire = Time.time + 1f / fireRate;
+            Shoot();
+
+        }
+    
+
+    }
+
+    void Shoot ()
+    {
+        RaycastHit hit;
+        if (Physics.Raycast(fpsCam.transform.position, fpsCam.transform.forward, out hit, range))
+        {
+
+            Debug.Log(hit.transform.name);
+
+            Target target = hit.transform.GetComponent<Target>();
+            if (target != null)
+            {
+                target.TakeDamage(damage);
+            }
+        }
+
+    }
+}
