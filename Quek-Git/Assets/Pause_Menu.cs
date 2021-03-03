461ff38c4706c66803cba2f5497354aa49d8dbb6
RainyFluff <nikodemius.ivarsson@elev.ga.lbs.se>
2021-03-03 15:09:49 +0100

    v.9
    cum

---
+++ Quek-Git/Assets/Pause_Menu.cs
@@ -0,0 +1,59 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.SceneManagement;
+public class Pause_Menu : MonoBehaviour
+{
+    // Start is called before the first frame update
+
+    public static bool GameIsPaused = false;
+    public GameObject PauseMenuUI;
+    public GameObject Script;
+    // Update is called once per frame
+    void Update()
+    {
+        if (Input.GetKeyDown(KeyCode.Escape))
+        {
+
+            if (GameIsPaused)
+            {
+                Resume();
+            }
+            else
+            {
+                Pause();
+            }
+        }
+    }
+
+    public void Resume()
+    {
+        PauseMenuUI.SetActive(false);
+        Time.timeScale = 1f;
+        GameIsPaused = false;
+        Script.SetActive(true);
+        Cursor.lockState = CursorLockMode.Locked;
+        Cursor.visible = false;
+    }
+
+    void Pause()
+    {
+        PauseMenuUI.SetActive(true);
+        Time.timeScale = 0f;
+        GameIsPaused = true;
+        Script.SetActive(false);
+        Cursor.lockState = CursorLockMode.None;
+        Cursor.visible = true;
+    }
+
+    public void LoadMenu()
+    {
+        SceneManager.LoadScene("Main");
+        Time.timeScale = 1f;
+    }
+
+    public void Quit()
+    {
+        Application.Quit();
+    }
+}
---
+++ Quek-Git/Assets/Scripts/SLOWMOTION.cs
@@ -0,0 +1,50 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class SLOWMOTION : MonoBehaviour
+{
+    public float SlowMotion;
+    public static bool HeldDown = false;
+    
+    // Start is called before the first frame update
+    void Start()
+    {
+        Time.fixedDeltaTime = Time.timeScale * .02f;
+
+
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        if (Input.GetKeyDown(KeyCode.Mouse2))
+        {
+            if (HeldDown)
+            {
+                Normal();
+            }
+            else
+            {
+                SlowDown();
+            }
+            
+        }
+        
+    }   
+
+    void SlowDown()
+    {
+        Time.timeScale = SlowMotion;
+        HeldDown = true;
+        Time.fixedDeltaTime = Time.timeScale * .02f;
+    }
+
+    void Normal()
+    {
+        Time.timeScale = 1f;
+        HeldDown = false;
+        Time.fixedDeltaTime = .02f;
+    }
+
+}
